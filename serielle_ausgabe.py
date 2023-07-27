b4569e7048c6fbd93aa2c11264ce71ac92c14bf4
Hairocketfire <Jonas.berst@t-online.de>
2023-07-28 00:07:35 +0200

    Version 1

---
+++ serielle_ausgabe.py
@@ -0,0 +1,45 @@
+import serial
+import time
+#i want to play a sound when someone is detected
+import os
+
+
+
+
+
+# Verbinde dich mit der seriellen Schnittstelle (angepasste Einstellungen verwenden)
+ser = serial.Serial("COM13", baudrate=115200, timeout=1)
+
+
+# Die aktuelle Zeit im Format "Stunde:Minute:Sekunde" ausgeben
+
+
+try:
+    while True:
+        # Lies die Daten aus der seriellen Schnittstelle
+        data = ser.readline().decode().strip()
+
+        # Gib die empfangenen Daten auf der Konsole aus
+
+        if data == "1":
+            # Play a audio file when someone is detected
+            # play a audio file when someone is detected
+            
+            os.system ("Person_alarm.mp3")
+            
+
+           
+
+            
+            
+            aktuelle_zeit = time.time()
+            print(
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(aktuelle_zeit)),
+                "|",
+                "Person Gesichtet",
+            )
+
+
+except KeyboardInterrupt:
+    # Schließe die serielle Verbindung, wenn das Programm beendet wird
+    ser.close()
